8c647cf17c48567266c760cf60907085b81e5604
CuentaBorrada <amateo1982@yahoo.es>
2021-02-26 00:06:01 +0100

    Agregado el listado de películas recuperando la info con fetch desde la API. Agregada también la paginación.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,121 @@
 import "./App.css";
 import "bulma/css/bulma.css";
-import { Title } from "./components/Title";
+import { Title } from "./components/ui/Title";
+import { SearchForm } from "./components/SearchForm";
+import { useState } from "react";
 
 function App() {
+  const apiKey = "4081eee7cd72cb08acc0d2f49deec1da";
+  const [currentPage, setCurrentPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+  const [pagination, setPagination] = useState([]);
+
+  const showResults = (data) => {
+    setMovies(data.results);
+    const arrayForPages = Array.from(Array(data.total_pages).keys());
+    setPagination(arrayForPages);
+  };
+
+  const _handleClickPage = (event) => {
+    const currentValueButton = Number(event.target.innerHTML);
+    setCurrentPage(currentValueButton);
+  };
+
   return (
     <div className="container">
       <div className="app">
         <Title>Search Movies</Title>
+
         <br />
-        <button className="button is-danger">Danger</button>
+        <div className="is-flex is-justify-content-center">
+          <SearchForm submitResults={showResults} apiKey={apiKey} />
+        </div>
+
+        <div>
+          <ul className="movies-list">
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <span
+                  className="movies-list__img"
+                  style={{
+                    backgroundImage: movie.poster_path
+                      ? `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`
+                      : `url(${process.env.PUBLIC_URL}/default-movie.png)`,
+                  }}
+                ></span>
+                <span className="movies-list__title">{movie.title}</span>
+              </li>
+            ))}
+          </ul>
+          <nav
+            className="pagination is-centered"
+            role="navigation"
+            aria-label="pagination"
+          >
+            <ul className="pagination-list">
+              <li style={{ display: currentPage === 1 && "none" }}>
+                <button
+                  type="button"
+                  className="pagination-link"
+                  onClick={_handleClickPage}
+                >
+                  1
+                </button>
+              </li>
+              <li>
+                <span className="pagination-ellipsis">&hellip;</span>
+              </li>
+              <li style={{ display: currentPage === 1 && "none" }}>
+                <button
+                  type="button"
+                  className="pagination-link"
+                  onClick={_handleClickPage}
+                >
+                  {currentPage - 1}
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="pagination-link is-current"
+                  aria-current="page"
+                  onClick={_handleClickPage}
+                >
+                  {currentPage}
+                </button>
+              </li>
+              <li
+                style={{
+                  display: currentPage + 1 > pagination.length && "none",
+                }}
+              >
+                <button
+                  type="button"
+                  className="pagination-link"
+                  onClick={_handleClickPage}
+                >
+                  {currentPage + 1}
+                </button>
+              </li>
+              <li>
+                <span className="pagination-ellipsis">&hellip;</span>
+              </li>
+              <li
+                style={{
+                  display: currentPage + 1 >= pagination.length && "none",
+                }}
+              >
+                <button
+                  type="button"
+                  className="pagination-link"
+                  onClick={_handleClickPage}
+                >
+                  {pagination.length}
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SearchForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export class SearchForm extends Component {
+  state = {
+    inputValue: "",
+  };
+
+  _handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${this.props.apiKey}&query=${this.state.inputValue}&page=2&include_adult=false`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.props.submitResults(data);
+      })
+      .catch((err) => {
+        throw new Error(`Error: ${err}`);
+      });
+  };
+
+  _handleChange = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this._handleSubmit}>
+          <div className="field has-addons is-flex is-justify-content-center">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                onChange={this._handleChange}
+                placeholder="Busca una película"
+              />
+            </div>
+            <div className="control">
+              <button type="submit" className="button is-info">
+                Buscar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
