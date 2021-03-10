59bfabc89e61ba4f3e06ff0f1dc3a291cd98151e
CuentaBorrada <amateo1982@yahoo.es>
2021-03-10 22:56:08 +0100

    Modificaciones en los tests del componente Pagination

--- src/components/ui/Pagination.spec.js
+++ src/components/ui/Pagination.spec.js
@@ -2,38 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Pagination from "../ui/Pagination";
 
-test("Pagination component testing", () => {
-  const handleClickPage = () => {};
-  const currentPage = 1;
-  const paginationLength = 200;
+describe("Pagination Component Tests", () => {
+  test("Component should render correctly", () => {
+    const handleClickPage = () => {};
+    const currentPage = 1;
+    const paginationLength = 200;
 
-  render(
-    <Pagination
-      currentPage={currentPage}
-      paginationLength={paginationLength}
-      handleClickPage={handleClickPage}
-    />
-  );
-
-  const doNotShowFirstPage = () => {
-    return currentPage === 1 || currentPage === 2;
-  };
-  expect(doNotShowFirstPage()).toBeTruthy();
-
-  const doNotShowPrevPage = () => {
-    return currentPage === 1;
-  };
-  expect(doNotShowPrevPage()).toBeTruthy();
-
-  const doNotShowNextPage = () => {
-    return currentPage + 1 > paginationLength;
-  };
-  expect(doNotShowNextPage()).toBeFalsy();
-
-  const doNotShowLastPage = () => {
-    return (
-      currentPage === paginationLength || currentPage === paginationLength - 1
+    const { asFragment, unmount } = render(
+      <Pagination
+        currentPage={currentPage}
+        paginationLength={paginationLength}
+        handleClickPage={handleClickPage}
+      />
     );
-  };
-  expect(doNotShowLastPage()).toBeFalsy();
+
+    const firstRender = asFragment();
+    expect(firstRender).toMatchSnapshot(asFragment());
+    unmount();
+  });
 });
