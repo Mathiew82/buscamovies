de4d3992e8008dd63a2f48e2a844c2cc9cda1a84
CuentaBorrada <amateo1982@yahoo.es>
2021-04-14 01:33:22 +0200

    Refactor del test del componente Label

--- src/components/ui/Label/Label.test.js
+++ src/components/ui/Label/Label.test.js
@@ -5,8 +5,8 @@ import Label from './Label'
 /** Variables for testing */
 const text = 'Test'
 
-describe('label', () => {
-  it('component should render correctly', () => {
+describe('Label Component', () => {
+  it('should render correctly', () => {
     render(<Label>{text}</Label>)
 
     expect(screen.getByText(text)).toBeInTheDocument()
