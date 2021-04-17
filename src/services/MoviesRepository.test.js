c23dac4a579946cf2d0d57ebfe434a099911af33
CuentaBorrada <amateo1982@yahoo.es>
2021-04-17 03:11:36 +0200

    Agregado el test del componente MoviesRepository

---
+++ src/services/MoviesRepository.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+  searchMovie,
+  searchMovies,
+  searchPopularMovies,
+  searchCredits,
+} from './MoviesRepository'
+
+describe('MoviesRepository', () => {
+  it('should execute correctly the searchMovie method', async () => {
+    const testURL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+    let response = null
+    await searchMovie(testURL).then((data) => {
+      response = data
+    })
+
+    expect(typeof response).toBe('object')
+  })
+
+  it('should execute correctly the searchMovies method', async () => {
+    const testURL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+    let response = null
+    await searchMovies(testURL).then((data) => {
+      response = data
+    })
+
+    expect(typeof response).toBe('object')
+  })
+
+  it('should execute correctly the searchPopularMovies method', async () => {
+    const testURL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+    let response = null
+    await searchPopularMovies(testURL).then((data) => {
+      response = data
+    })
+
+    expect(typeof response).toBe('object')
+  })
+
+  it('should execute correctly the searchCredits method', async () => {
+    const testURL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+    let response = null
+    await searchCredits(testURL).then((data) => {
+      response = data
+    })
+
+    expect(typeof response).toBe('object')
+  })
+})
