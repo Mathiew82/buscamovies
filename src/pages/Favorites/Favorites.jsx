83378eb7c8345783f134809525f4fa73d8c38b5c
Mathiew82 <amateo1982@yahoo.es>
2026-01-04 04:12:07 +0100

    refactor(components): move Header and Footer out of ui and move Loading into ui

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import Header from '@/components/ui/Header/Header'
-import Footer from '@/components/ui/Footer/Footer'
-import Title from '@/components/ui/Title/Title'
+import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 import Movie from '@/components/Movie/Movie'
+import Title from '@/components/ui/Title/Title'
 
 function Favorites() {
   const [movies, setMovies] = useState([])
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-import Header from '@/components/ui/Header/Header'
-import Footer from '@/components/ui/Footer/Footer'
+import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 import MoviesListContainer from '@/components/MoviesList/container/MoviesListContainer'
 
 function Home() {
