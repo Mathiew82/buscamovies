e42aa6f59b9acdc91d18cda9d9d2ee0952c9868b
Mathiew82 <amateo1982@yahoo.es>
2026-01-05 04:11:52 +0100

    style: change title and add a svg when there are no results

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -25,7 +25,7 @@ function Favorites() {
   return (
     <div className="favorites-page">
       <Header />
-      <Title>Mis Favoritos</Title>
+      <Title>Mis Favoritas</Title>
 
       {movies.length > 0 ? (
         <ul className="movies-list">
@@ -35,6 +35,23 @@ function Favorites() {
         </ul>
       ) : (
         <div className="no-movie-results">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-frown-icon lucide-frown"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M16 16s-1.5-2-4-2-4 2-4 2" />
+            <line x1="9" x2="9.01" y1="9" y2="9" />
+            <line x1="15" x2="15.01" y1="9" y2="9" />
+          </svg>
           No tienes películas agregadas a favoritos
         </div>
       )}
